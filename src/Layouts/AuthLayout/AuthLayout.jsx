fcbf4eb1a2d1f4eb9805877bd4b8a78274442d70
Muntasir <alimuntasir2001@gmail.com>
2026-01-28 00:21:50 +0600

    SignIn page Started

--- src/Layouts/AuthLayout/AuthLayout.jsx
+++ src/Layouts/AuthLayout/AuthLayout.jsx
@@ -3,30 +3,29 @@ import { Outlet } from 'react-router';
 import AuthImage from "../../assets/authImage.png";
 import DakBox from '../../Pages/SharedCopmponents/DakBoxLogo/DakBox';
 
-
 const AuthLayout = () => {
     return (
-        <div className="flex flex-col lg:flex-row min-h-screen bg-white">
-            {/* Left Side: Form Content */}
-            <div className="w-full lg:w-1/2 flex flex-col p-8 md:p-16 lg:p-24">
-                {/* Logo Import */}
-                <div className="mb-12">
-                    <DakBox />
-                </div>
+        <div className="flex min-h-screen bg-white relative overflow-hidden">
+            
+            {/* Logo: Positioned at Top Left Corner */}
+            <div className="absolute top-6 left-6 md:top-10 md:left-10 z-50">
+                <DakBox />
+            </div>
 
-                {/* Dynamic Content (Login/Register Form) */}
-                <div className="w-full max-w-md mx-auto lg:mx-0">
+            {/* Left Side: Form Content (Strictly 50%) */}
+            <div className="w-full lg:w-1/2 flex items-center justify-center p-6 md:p-12">
+                <div className="w-full max-w-md">
                     <Outlet />
                 </div>
             </div>
 
-            {/* Right Side: Image Illustration */}
-            <div className="hidden lg:flex w-1/2 bg-[#F1F9E8] items-center justify-center p-12">
-                <div className="relative w-full max-w-lg">
+            {/* Right Side: Image Illustration (Strictly 50% No Gaps) */}
+            <div className="hidden lg:flex w-1/2 bg-[#F1F9E8] items-center justify-center relative">
+                <div className="w-full h-full flex items-center justify-center p-16">
                     <img 
                         src={AuthImage} 
                         alt="Authentication Illustration" 
-                        className="w-full h-auto object-contain"
+                        className="max-w-full max-h-[80vh] object-contain"
                     />
                 </div>
             </div>
--- src/Pages/AuthPages/SignIn/SignIn.jsx
+++ src/Pages/AuthPages/SignIn/SignIn.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const SignIn = () => {
-    return (
-        <div>
-            ami sign in
-        </div>
-    );
+
+    const {register ,handleSubmit} = useForm();
+
+     const onSubmit = data => {
+        console.log(data);
+     };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="form-control max-w-md mx-auto mt-10 p-8 border border-gray-300 rounded-lg shadow-lg">
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" {...register('email')} className="input" placeholder="Email" />
+
+          <label className="label">Password</label>
+          <input type="password" {...register('password')} className="input" placeholder="Password" />
+          
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+         
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
