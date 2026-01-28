aaed0c3b8ad341222c959139c5bd73301a6cfeba
Muntasir <alimuntasir2001@gmail.com>
2026-01-28 21:24:58 +0600

    AuthPage Firebase started

--- src/Layouts/AuthLayout/AuthLayout.jsx
+++ src/Layouts/AuthLayout/AuthLayout.jsx
@@ -7,16 +7,13 @@ const AuthLayout = () => {
     return (
         <div className="flex min-h-screen bg-white relative overflow-hidden">
             
-            {/* Logo: Positioned at Top Left */}
-            {/* md:top-10 এবং md:left-10 স্পেসিং বাড়ানো হয়েছে */}
+            
             <div className="absolute top-6 left-6 md:top-10 md:left-12 z-50">
                 <Link to="/">
                     <DakBox />
                 </Link>
             </div>
-
-            {/* Left Side: Form Content */}
-            {/* pt-32 বা pt-40 যোগ করা হয়েছে যাতে লোগোর নিচে ফর্ম শুরু হয় */}
+            {/* Left Side: Form Section */}
             <div className="w-full lg:w-1/2 flex items-center justify-center p-6 md:p-12 pt-32 md:pt-40">
                 <div className="w-full max-w-md">
                     <Outlet />
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router/dom";
 import { router } from './Router/Router';
+import Aos from 'aos';
+import AuthProvider from './CONTEXT/AuthContext/AuthProvider';
 
+Aos.init();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <div className='font-urbanist max-w-auto mx-auto'>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
     </div>
   </StrictMode>
 )
